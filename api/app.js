0ecfbf166e820bf0132e588fca8a4c258b7a205d
Austin Cai <acai21@stanford.edu>
2020-03-27 15:02:40 -0700

    finished tutorial, react client and espress api set up

---
+++ api/app.js
@@ -0,0 +1,54 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require("cors");
+<<<<<<< HEAD
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var testAPIRouter = require("./routes/testAPI");
+=======
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var testAPIRouter = require("./routes/testAPI");
+
+>>>>>>> 01f986de382b29b99ccd30cfd706c38d2cd03fc2
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use("/testAPI", testAPIRouter);
+<<<<<<< HEAD
+=======
+
+>>>>>>> 01f986de382b29b99ccd30cfd706c38d2cd03fc2
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ api/routes/testAPI.js
@@ -0,0 +1,19 @@
+<<<<<<< HEAD
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+    res.send('API is working properly. Hello world from Austin!');
+});
+
+module.exports = router;
+=======
+var express = require("express");
+var router = express.Router();
+
+router.get("/", function(req, res, next) {
+    res.send("API is working properly");
+});
+
+module.exports = router;
+>>>>>>> 01f986de382b29b99ccd30cfd706c38d2cd03fc2
