d537ac0d14b520152e11f5732f673ca9b07ffb44
Lydia Chan <lchan528@stanford.edu>
2020-04-01 15:45:16 -0700

    Added form data handling for login

--- api/routes/login.js
+++ api/routes/login.js
@@ -1,26 +1,47 @@
 var express = require("express");
 var router = express.Router();
+var multer = require("multer");
+var upload = multer();
 
 let accounts = [];
+let accountId = 0;
 
 function checkLogin(query){
   if (query.username && query.password){
-    return query;
+    return {
+      id: accountId++,
+      username: query.username,
+      password: query.password
+    };
   } else {
     return -1;
   }
 }
 
-router.post("/", function(req, res, next) {
-  const receivedInfo = checkLogin(req.query);
+router.use(upload.array());
+router.post("/", (req, res, next) => {
+  // console.log(req.body.username, req.body.password);
+  const receivedInfo = checkLogin(req.body);
   if (receivedInfo !== -1) {
     accounts.push(receivedInfo);
     res.status(201).send(receivedInfo);
     console.log("success");
     console.log(accounts);
   } else {
-    res.status(400).send();
+    res.status(404).send();
   }
 });
+
+// router.post("/", function(req, res, next) {
+//   const receivedInfo = checkLogin(req.query);
+//   if (receivedInfo !== -1) {
+//     accounts.push(receivedInfo);
+//     res.status(201).send(receivedInfo);
+//     console.log("success");
+//     console.log(accounts);
+//   } else {
+//     res.status(400).send();
+//   }
+// });
 
 module.exports = router;
