8de457d3a4d805f379b789121c96f918a58af0eb
Austin Cai <acai21@stanford.edu>
2020-04-07 18:57:36 -0700

    got chat working properly, started working on forum

--- client/src/HomeScreen.js
+++ client/src/HomeScreen.js
@@ -3,11 +3,53 @@ import Login from './Login';
 import ChatApp from './ChatApp';
 import '@progress/kendo-theme-material/dist/all.css';
 
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+
+class Forum extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      forumPosts: null
+    };
+  }
+
+  async getForumPosts() {
+    try {
+      const res = await fetch("http://localhost:9000/forum/tag/food");
+      if (res.ok){
+        const resText = await res.text();
+        this.setState({forumPosts: resText})
+      } else {
+        throw new Error('Request failed!');
+      }
+    } catch (error){
+      console.log(error);
+    }
+  }
+
+  componentDidMount(){
+    this.getForumPosts();
+  }
+
+  render() {
+    console.log(this.state.forumPosts);
+    return (
+      <div>
+        <h1>Forum</h1>
+        <br/>
+        <p>{this.state.forumPosts}</p>
+      </div>
+    );
+  }
+}
+
 export class HomeScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: '',
+      username: 'test',
       loggedIn: false
     };
     this.handleLogin = this.handleLogin.bind(this);
@@ -23,23 +65,19 @@ export class HomeScreen extends React.Component {
     this.setState({ username: event.target.value });
   }
 
+
   render() {
-    let loginOrChat;
-    if (this.state.loggedIn) {
-      loginOrChat = <ChatApp username={this.state.username}/>;
-    } else {
-      loginOrChat = (
-        <Login
-          handleLogin={this.handleLogin}
-          handleUsernameChange={this.handleUsernameChange}
-          username={this.state.username}
-        />
-      );
-    }
     return (
-      <div className="container">
-        <div className="row mt-3 justify-content-center">{loginOrChat}</div>
-      </div>
+      <Row>
+        <Col md={6}>
+          <div className="row mt-3 justify-content-center"><Forum/></div>
+        </Col>
+
+        <Col md={6} className="container">
+          <div className="row mt-3 justify-content-center"><h1>Global Chat</h1></div>
+          <div className="row mt-3 justify-content-center"><ChatApp username={this.props.profileInfo.firstName}/></div>
+        </Col>
+      </Row>
     );
   }
 }
